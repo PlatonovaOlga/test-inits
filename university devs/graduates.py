6df68b274af44f361b06abc41b11a1ebc3aad484
PlatonovaOlga <ov_pl@bk.ru>
2022-04-06 13:49:28 +0300

    return link

--- university devs/graduates.py
+++ university devs/graduates.py
@@ -1,9 +1,10 @@
 """
 Find and save in an excel-compatible file the top three graduates.
-The function should return the name of the created file 
+The function should return link to created file 
 """
 
 from heapq import nlargest
+import os
 import pandas as pd
 
 from data import gp_avg
@@ -13,7 +14,8 @@ def make_report_top3(gp_avg):
     file_name = 'report.xlsx'
     df = pd.DataFrame(nlargest(3, gp_avg, key=gp_avg.get))
     df.to_excel(file_name)
-    return file_name
+    link = os.path.abspath(os.getcwd()) + '\\' + file_name
+    return link
 
 
 best_graduates = make_report_top3(gp_avg)
